273f6cf8d7452179db70e94d595bc96ca4606a23
LyraxDev <134977916+LyraxDev@users.noreply.github.com>
2024-06-10 20:00:28 -0300

    Update GlobalContext.js

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -15,10 +15,14 @@ export const GlobalStorage = ({ children }) => {
   const [typeBuy, setTypeBuy] = React.useState('');
   const [cep, setCep] = React.useState('');
   const [number, setNumber] = React.useState('');
+  const [cel, setCelular] = React.useState('');
   const [complement, setComplement] = React.useState('');
   const [typePayment, setTypePayment] = React.useState('');
   const [address, setAddress] = React.useState(null);
   const [order, setOrder] = React.useState('');
+  const [rua, setRua] = React.useState('');
+  const [bairro, setBairro] = React.useState('');
+  const [name, setName] = React.useState('');
 
   function addCart(item) {
     item.quantity = 1;
@@ -27,7 +31,6 @@ export const GlobalStorage = ({ children }) => {
     setCart(oldArray => [...oldArray, item]);
   }
 
-  // add note to cart item
   function addObs(id) {
     let indexItem = cart.map((e) => e.id).indexOf(id);
     let updatedCart = [...cart];
@@ -56,85 +59,83 @@ export const GlobalStorage = ({ children }) => {
     setCart(updatedCart);
   }
 
-
   const getProducts = async (category) => {
     let jsonData;
     if (category === "Esfihas") {
       jsonData = [
         {
-            "id": 1,
-            "title": "Frango com Catupiry",
-            "description": "Molho de tomate, frango, milho, mussarela, catupiry e orégano",
-            "price": 8,
-            },
+          "id": 1,
+          "title": "Frango com Catupiry",
+          "description": "Molho de tomate, frango, milho, mussarela, catupiry e orégano",
+          "price": 8,
+        },
         {
-            "id": 2,
-            "title": "Frango com Cheddar",
-            "description": "Molho de tomate, frango, milho, mussarela, cheddar e orégano",
-            "price": 8,
-            },
+          "id": 2,
+          "title": "Frango com Cheddar",
+          "description": "Molho de tomate, frango, milho, mussarela, cheddar e orégano",
+          "price": 8,
+        },
         {
-            "id": 3,
-            "title": "Franbacon",
-            "description": "Molho de tomate, frango, milho, bacon, mussarela e orégano",
-            "price": 8,
-            },
+          "id": 3,
+          "title": "Franbacon",
+          "description": "Molho de tomate, frango, milho, bacon, mussarela e orégano",
+          "price": 8,
+        },
         {
-            "id": 4,
-            "title": "Frango",
-            "description": "Molho de tomate, frango, milho, mussarela e orégano",
-            "price": 8,
-            },
+          "id": 4,
+          "title": "Frango",
+          "description": "Molho de tomate, frango, milho, mussarela e orégano",
+          "price": 8,
+        },
         {
-            "id": 5,
-            "title": "Nordestina 1",
-            "description": "Molho de tomate, carne de sol, cebola, mussarela e orégano",
-            "price": 8,
-            },
+          "id": 5,
+          "title": "Nordestina 1",
+          "description": "Molho de tomate, carne de sol, cebola, mussarela e orégano",
+          "price": 8,
+        },
         {
-            "id": 6,
-            "title": "Nordestina 2",
-            "description": "Molho de tomate, carne de sol, cebola, mussarela, cheddar e orégano",
-            "price": 8,
-            },
+          "id": 6,
+          "title": "Nordestina 2",
+          "description": "Molho de tomate, carne de sol, cebola, mussarela, cheddar e orégano",
+          "price": 8,
+        },
         {
-            "id": 7,
-            "title": "Calabresa",
-            "description": "Molho de tomate, calabresa, cebola, mussarela e orégano",
-            "price": 8,
-            },
+          "id": 7,
+          "title": "Calabresa",
+          "description": "Molho de tomate, calabresa, cebola, mussarela e orégano",
+          "price": 8,
+        },
         {
-            "id": 8,
-            "title": "Bacon",
-            "description": "Molho de tomate, bacon, cebola, mussarela e orégano",
-            "price": 8,
-            },
+          "id": 8,
+          "title": "Bacon",
+          "description": "Molho de tomate, bacon, cebola, mussarela e orégano",
+          "price": 8,
+        },
         {
-            "id": 9,
-            "title": "Calabacon",
-            "description": "Molho de tomate, calabresa, bacon, cebola, mussarela e orégano",
-            "price": 8,
-            },
+          "id": 9,
+          "title": "Calabacon",
+          "description": "Molho de tomate, calabresa, bacon, cebola, mussarela e orégano",
+          "price": 8,
+        },
         {
-            "id": 10,
-            "title": "3 Queijos",
-            "description": "Molho de tomate, mussarela, catupiry, cheddar, batata palha e orégano",
-            "price": 8,
-            },
+          "id": 10,
+          "title": "3 Queijos",
+          "description": "Molho de tomate, mussarela, catupiry, cheddar, batata palha e orégano",
+          "price": 8,
+        },
         {
-            "id": 11,
-            "title": "Mista",
-            "description": "Molho de tomate, presunto, mussarela e orégano",
-            "price": 8,
-            },
+          "id": 11,
+          "title": "Mista",
+          "description": "Molho de tomate, presunto, mussarela e orégano",
+          "price": 8,
+        },
         {
-            "id": 12,
-            "title": "À Moda da Casa",
-            "description": "Molho de tomate, calabresa, bacon, tomate, cebola e orégano",
-            "price": 8,
-            }
-    ]
-    
+          "id": 12,
+          "title": "À Moda da Casa",
+          "description": "Molho de tomate, calabresa, bacon, tomate, cebola e orégano",
+          "price": 8,
+        }
+      ]
     } else if (category === "Bebidas") {
       jsonData = [
         {
@@ -144,8 +145,6 @@ export const GlobalStorage = ({ children }) => {
           "price": null
         }
       ]
-      
-    
     } else {
       jsonData = [
         {
@@ -159,13 +158,9 @@ export const GlobalStorage = ({ children }) => {
           "price": 5.99
         },
       ];
-
     }
-  
-    // Use os dados diretamente
     setData(jsonData);
   }
-  
 
   async function getCep(cep) {
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
@@ -179,14 +174,12 @@ export const GlobalStorage = ({ children }) => {
     });
   }
 
-  // wait for the CEP to be filled in with 8 digits to make the request
   React.useEffect(() => {
     if (cep.length >= 8) {
       getCep(cep);
     }
-  }, [cep])
+  }, [cep]);
 
-  // watch the cart changes to calculate the total price.
   React.useEffect(() => {
     if (cart.length > 0) {
       const prices = cart.map((item) => item.currentPrice);
@@ -194,7 +187,6 @@ export const GlobalStorage = ({ children }) => {
     }
   }, [cart]);
 
-  // save the address in localStorage after finalizing the order
   React.useEffect(() => {
     if (typeBuy === 'delivery') {
       window.localStorage.setItem('address', JSON.stringify(order.address));
@@ -205,100 +197,60 @@ export const GlobalStorage = ({ children }) => {
     setListProducts(data);
   }, [data]);
 
-  // loads product list and default localStorage address
   React.useEffect(() => {
     async function loadData() {
-      let jsonData;
-
-      jsonData = [
-        {
-            "id": 1,
-            "title": "Frango com Catupiry",
-            "description": "Molho de tomate, frango, milho, mussarela, catupiry e orégano",
-            "price": 8,
-            },
-        {
-            "id": 2,
-            "title": "Frango com Cheddar",
-            "description": "Molho de tomate, frango, milho, mussarela, cheddar e orégano",
-            "price": 8,
-            },
-        {
-            "id": 3,
-            "title": "Franbacon",
-            "description": "Molho de tomate, frango, milho, bacon, mussarela e orégano",
-            "price": 8,
-            },
-        {
-            "id": 4,
-            "title": "Frango",
-            "description": "Molho de tomate, frango, milho, mussarela e orégano",
-            "price": 8,
-            },
-        {
-            "id": 5,
-            "title": "Nordestina 1",
-            "description": "Molho de tomate, carne de sol, cebola, mussarela e orégano",
-            "price": 8,
-            },
-        {
-            "id": 6,
-            "title": "Nordestina 2",
-            "description": "Molho de tomate, carne de sol, cebola, mussarela, cheddar e orégano",
-            "price": 8,
-            },
-        {
-            "id": 7,
-            "title": "Calabresa",
-            "description": "Molho de tomate, calabresa, cebola, mussarela e orégano",
-            "price": 8,
-            },
-        {
-            "id": 8,
-            "title": "Bacon",
-            "description": "Molho de tomate, bacon, cebola, mussarela e orégano",
-            "price": 8,
-            },
-        {
-            "id": 9,
-            "title": "Calabacon",
-            "description": "Molho de tomate, calabresa, bacon, cebola, mussarela e orégano",
-            "price": 8,
-            },
-        {
-            "id": 10,
-            "title": "3 Queijos",
-            "description": "Molho de tomate, mussarela, catupiry, cheddar, batata palha e orégano",
-            "price": 8,
-            },
-        {
-            "id": 11,
-            "title": "Mista",
-            "description": "Molho de tomate, presunto, mussarela e orégano",
-            "price": 8,
-            },
-        {
-            "id": 12,
-            "title": "À Moda da Casa",
-            "description": "Molho de tomate, calabresa, bacon, tomate, cebola e orégano",
-            "price": 8,
-            }
-    ]
-
-      setData(jsonData);
+      let jsonData = [
+        // Your default data
+      ];
+      setListProducts(jsonData);
     }
     loadData();
-    const addressDefault = window.localStorage.getItem('address');
-    if (addressDefault !== '' && addressDefault !== null && addressDefault !== undefined) {
-      setUser(JSON.parse(addressDefault));
-    } else {
-      setUser(null);
-    }
   }, []);
 
   return (
-    <GlobalContext.Provider value={{ getProducts, listProducts, addCart, cart, total, incrementItem, decrementItem, user, setUser, typeBuy, setTypeBuy, cep, setCep, number, setNumber, complement, setComplement, typePayment, setTypePayment, address, setAddress, order, setOrder, openCart, setOpenCart, openObs, setOpenObs, idObs, setIdObs, obs, setObs, addObs }}>
+    <GlobalContext.Provider value={{
+      getProducts,
+      listProducts,
+      addCart,
+      cart,
+      total,
+      incrementItem,
+      decrementItem,
+      user,
+      setUser,
+      typeBuy,
+      setTypeBuy,
+      cep,
+      setCep,
+      number,
+      setNumber,
+      complement,
+      setComplement,
+      cel,
+      setCelular,
+      typePayment,
+      setTypePayment,
+      address,
+      setAddress,
+      order,
+      setOrder,
+      name,
+      setName,
+      openCart,
+      setOpenCart,
+      openObs,
+      setOpenObs,
+      idObs,
+      setIdObs,
+      obs,
+      setObs,
+      addObs,
+      rua,
+      setRua,
+      bairro,
+      setBairro
+    }}>
       {children}
     </GlobalContext.Provider>
   );
-};
+}
